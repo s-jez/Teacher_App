129ca3991093c078349f0cfcd9d7dbf3a7f45d46
Stachowsky <stachowskycode@gmail.com>
2021-08-19 19:17:49 +0200

    add controllers functions

--- controllers/students.go
+++ controllers/students.go
@@ -1,4 +1,19 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
 // Implement CRUD controllers to work with database and perform CRUD operations..
 // Create, Read, Update, Delete
+
+func CreateStudent(c *gin.Context) {
+	// Create operations
+}
+func ReadStudent(c *gin.Context) {
+	// Read operations
+}
+func UpdateStudent(c *gin.Context) {
+	// Update operations
+}
+func DeleteStudent(c *gin.Context) {
+	// Delete oeprations
+}
