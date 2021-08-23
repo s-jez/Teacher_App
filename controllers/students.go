2bcaef1145b8cc89af01d27c93e6c9c858c38494
Stachowsky <stachowskycode@gmail.com>
2021-08-23 15:59:24 +0200

    crud changes

--- controllers/students.go
+++ controllers/students.go
@@ -9,14 +9,19 @@ import (
 
 func CreateStudent(c *gin.Context) {
 	var student models.Student
+
 	err := c.BindJSON(&student)
 	if err != nil {
 		c.JSON(400, err.Error())
 	}
+
 	if err := models.AddStudent(&student); err != nil {
 		c.JSON(500, err.Error())
 		return
 	}
+	// student.FirstName = c.PostForm("FirstName")
+	// student.LastName = c.PostForm("LastName")
+
 	c.JSON(http.StatusCreated, gin.H{
 		"message": "Student has been created!",
 		"data":    student,
@@ -24,6 +29,7 @@ func CreateStudent(c *gin.Context) {
 }
 func ReadStudents(c *gin.Context) {
 	var students []models.Student
+
 	if err := models.ReadStudents(&students); err != nil {
 		c.JSON(500, err.Error())
 		return
@@ -35,9 +41,19 @@ func ReadStudents(c *gin.Context) {
 }
 func ReadStudentById(c *gin.Context) {
 	var student models.Student
-	var id = c.Param("id")
-	if err := models.ReadStudentById(&student, id); err != nil {
+	id := c.Param("id")
+
+	err := c.BindJSON(&student)
+	if err != nil {
+		c.JSON(400, err.Error())
+		return
+	}
+
+	if err := models.ReadStudent(&student, id); err != nil {
 		c.JSON(500, err.Error())
+		return
+	}
+	if student.ID == 0 {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
@@ -47,8 +63,14 @@ func ReadStudentById(c *gin.Context) {
 }
 func DeleteStudentById(c *gin.Context) {
 	var student models.Student
-	var id = c.Param("id")
-	if err := models.DeleteStudentById(&student, id); err != nil {
+	id := c.Param("id")
+
+	err := c.BindJSON(&student)
+	if err != nil {
+		c.JSON(400, err.Error())
+	}
+
+	if err := models.DeleteStudent(&student, id); err != nil {
 		c.JSON(500, err.Error())
 		return
 	}
@@ -59,11 +81,29 @@ func DeleteStudentById(c *gin.Context) {
 }
 func UpdateStudentById(c *gin.Context) {
 	var student models.Student
-	var id = c.Param("id")
-	if err := models.UpdateStudentById(&student, id); err != nil {
+	id := c.Param("id")
+	models.SelectById(&student, id)
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "We not found this student!",
+			"data":    student,
+		})
+	}
+	var newStudent models.Student
+	err := c.BindJSON(&newStudent)
+	if err != nil {
+		c.JSON(400, err.Error())
+	}
+	student.FirstName = newStudent.FirstName
+	student.LastName = newStudent.LastName
+	student.Age = newStudent.Age
+	student.Grade = newStudent.Grade
+
+	if err := models.UpdateStudent(&newStudent, id); err != nil {
 		c.JSON(500, err.Error())
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Student has been updated!",
 		"data":    student,
--- models/functions_student.go
+++ models/functions_student.go
@@ -16,20 +16,28 @@ func ReadStudents(Students *[]Student) error {
 	}
 	return nil
 }
-func ReadStudentById(Student *Student, ID string) error {
-	if err := config.DB.First(&Student, ID).Find(&Student).Error; err != nil {
+func ReadStudent(Student *Student, ID string) error {
+	SelectById(Student, ID)
+	if err := config.DB.Find(&Student).Error; err != nil {
 		return err
 	}
 	return nil
 }
-func UpdateStudentById(Student *Student, ID string) error {
-	if err := config.DB.First(&Student, ID).Save(&Student).Error; err != nil {
+func UpdateStudent(Student *Student, ID string) error {
+	if err := config.DB.Where("id = ?", ID).Save(&Student).Error; err != nil {
 		return err
 	}
 	return nil
 }
-func DeleteStudentById(Student *Student, ID string) error {
-	if err := config.DB.First(&Student, ID).Delete(&Student).Error; err != nil {
+func DeleteStudent(Student *Student, ID string) error {
+	SelectById(Student, ID)
+	if err := config.DB.Delete(&Student).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func SelectById(Student *Student, ID string) error {
+	if err := config.DB.First(&Student, ID).Error; err != nil {
 		return err
 	}
 	return nil
