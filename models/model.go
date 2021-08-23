beb0053e0801dc1aa310e3604930b5eb2fd885e6
Stachowsky <stachowskycode@gmail.com>
2021-08-23 15:59:37 +0200

    add changes

--- models/model.go
+++ models/model.go
@@ -2,11 +2,11 @@ package models
 
 // Student model
 type Student struct {
-	ID        uint   `gorm:"primaryKey" json:"id" binding:"required"`
-	FirstName string `json:"firstname" binding:"required"`
-	LastName  string `json:"lastname" binding:"required"`
-	Age       int    `json:"age" binding:"required"`
-	Grade     int    `json:"grade"`
+	ID        uint   `gorm:"primaryKey" json:"id"`
+	FirstName string `json:"firstname" binding:"required" form:"firstname"`
+	LastName  string `json:"lastname" binding:"required" form:"lastname"`
+	Age       int    `json:"age" binding:"required" form:"age"`
+	Grade     int    `json:"grade" form:"grade"`
 }
 
 // Set student table name
