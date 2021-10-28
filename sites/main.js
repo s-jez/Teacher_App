b21d6c29fcd6127c8b21586dcca67a90e09c8286
Stachowsky <stasj30@gmail.com>
2021-10-28 11:33:38 +0200

    [frontend] - add fetch GET data

--- sites/main.js
+++ sites/main.js
@@ -1,8 +1,27 @@
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function() {
-    var json = JSON.parse(xhr.responseText);
-    console.log(json);
-    document.querySelector('.first').innerHTML = json.students.FirstName;
+const url_students = "/student";
+
+async function getapi(url)  {
+    // store response
+    const response = await fetch(url)
+    // store data in json
+    var data = await response.json();
+    console.log(data);
+    show(data);
 }
-xhttp.open("GET", "/student")
-xhttp.send()
+getapi(url_students);
+function show(data) {
+    let tab = ``;
+    for (let student of data) {
+        tab += `
+        <tr>
+        <th scope="row">${student.id}</th>
+        <td>${student.firstname} </td>
+        <td>${student.lastname} </td>
+        <td>${student.age} </td>
+        <td>${student.grade} </td>
+        <td><button type="button" class="btn btn-outline-primary">Edit</button></td>
+      <td><button type="button" class="btn btn-outline-danger">Delete</button></td>
+        </tr>`;
+    }
+    document.querySelector('.first').innerHTML = tab;
+}
