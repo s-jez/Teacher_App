72e4952e59520b0f620542be179a78f1ece2902e
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-19 15:44:52 +0100

    [dev] - add user functions

--- models/function_users.go
+++ models/function_users.go
@@ -1,1 +1,34 @@
 package models
+
+import "Stachowsky/Teacher_App/config"
+
+func CreateUser(User *User) error {
+	if err := config.DB.Table("users").Create(&User).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func CheckUserEmail(User *User, Email string) error {
+	if err := config.DB.Table("users").Where("Email = ?", Email).Find(&User).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func GetUserRole(User *User, userid uint64) error {
+	if err := config.DB.Table("users").Where("ID = ?", userid).Find(&User.RoleID).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func GetUserEmail(User *User, userid uint64) error {
+	if err := config.DB.Table("users").Where("ID = ?", userid).Find(&User.Email).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func GetUserId(User *User, userid uint64) error {
+	if err := config.DB.Table("users").Where("ID = ?", userid).Find(&User.ID).Error; err != nil {
+		return err
+	}
+	return nil
+}
