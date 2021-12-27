b36c2d2f179fea3e2880184fb5b5bae20562e738
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-27 15:08:54 +0100

    [dev] - comment Jquery code

--- sites/js/main.js
+++ sites/js/main.js
@@ -17,119 +17,159 @@ function show(data) {
         <td>${student.lastname} </td>
         <td>${student.age} </td>
         <td>${student.grade} </td>
-        <td><button type="button" id="btn_update" class="btn btn-outline-primary" data-id="${student.id}" data-target="#exampleModal2" data-toggle="modal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-      </svg>Edit</button></td>
-        <td><button type="button" id="btn_delete" class="btn btn-outline-danger" data-id="${student.id}" data-target="#exampleModal3" data-toggle="modal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-      </svg>Delete</button></td>
+        <td><button type="button" id="btn_update" class="btn btn-outline-primary" data-id="${student.id}" data-target="#exampleModal2" data-toggle="modal">Edit</button></td>
+        <td><button type="button" id="btn_delete" class="btn btn-outline-danger" data-id="${student.id}" data-target="#exampleModal3" data-toggle="modal">Delete</button></td>
         </tr>`;
   }
   document.querySelector('.first').innerHTML = tab;
 }
-// Add Student
-$(document).on('submit', '#myForm', function (event) {
-  event.preventDefault();
-  var formData = {
-    firstname: $("#firstname").val(),
-    lastname: $("#lastname").val(),
-    age: $("#age").val(),
-    grade: $("#grade").val(),
-  };
-  if (formData.firstname == "" || formData.lastname == "" || formData.age == 0 || formData.grade == 0) {
-    $('.info').empty();
-    $('.info').append('<div class="alert alert-danger">Please enter student data to form</div>');
-  } else {
-    $.ajax({
-      type: "POST",
-      url: "/student",
-      data: formData,
-      dataType: "JSON",
-      encode: true,
-      headers: {
-        "Authorization": "Bearer"
-      }
-    }).done(function () {
-      $('.info').empty();
-      getapi(url_students);
-      $('input#firstname').val('');
-      $('input#lastname').val('');
-      $('input#age').val('');
-      $('input#grade').val('');
-      $('#exampleModal').hide();
-      $('.close').click();
-      $('.info').addClass("success");
-      $('.info').append('<div class="alert alert-success">You have succesffully added a student!</div>');
-    }).fail(function () {
-      $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
-    });
-  }
-})
-// Delete Student
-$(document).on('click', '#btn_delete', function (event) {
-  event.preventDefault();
-  var id = $(this).attr('data-id');
-  $(document).on('click', '#delete-modal', function (event) {
-    event.preventDefault();
-    $('.info').empty();
-    $.ajax({
-      type: "DELETE",
-      url: "/student/" + id,
-      dataType: "JSON",
-      encode: true,
-    }).done(function () {
-      $('.info').empty();
-      getapi(url_students);
-      $('#exampleModal3').hide();
-      $('.close').click();
-      $('.info').addClass("success");
-      $('.info').append('<div class="alert alert-danger">You have successfully deleted a student!</div>');
-    }).fail(function () {
-      $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
-    });
-  })
-})
-// Update Student
-$(document).on('click', '#btn_update', function (event) {
-  event.preventDefault();
-  var id = $(this).attr('data-id');
-  $(document).on('click', '#update-modal', function (event) {
-    event.preventDefault();
-    $('.info').empty();
-    var formData = {
-      "firstname": $('#myForm2').find('input#firstname.form-control').val(),
-      "lastname": $('#myForm2').find('#lastname').val(),
-      "age": $('#myForm2').find('#age').val(),
-      "grade": $('#myForm2').find('#grade').val(),
-    };
-    firstname.value = $('tbody.first tr').attr('data-id');
-    if (formData.firstname == "" || formData.lastname == "" || formData.age == 0 || formData.grade == 0) {
-      $('.info').empty();
-      $('.info').append('<div class="alert alert-danger">Please enter student data to form</div>');
-    } else {
-      $.ajax({
-        type: "PUT",
-        url: "/student/" + id,
-        dataType: "JSON",
-        encode: true,
-        data: formData,
-      }).done(function (data) {
-        $('.info').empty();
-        getapi(url_students);
-        $('input#firstname').val('');
-        $('input#lastname').val('');
-        $('input#age').val('');
-        $('input#grade').val('');
-        $('#exampleModal2').hide();
-        $('.close').click();
-        $('.info').addClass("success");
-        $('.info').append('<div class="alert alert-primary">You have successfully updated a student!</div>');
-      }).fail(function () {
-        $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
-      }).catch(e => {
-        console.log(e);
-      });
-    }
-  })
-})
+// // Add Student
+// $(document).on('submit', '#formCreate', function (event) {
+//   event.preventDefault();
+//   var formData = {
+//     firstname: $("#formRegister__firstname").val(),
+//     lastname: $("#formRegister__lastname").val(),
+//     age: $("#formRegister__age").val(),
+//     grade: $("#formRegister__grade").val(),
+//   };
+//   if (formData.firstname == "" || formData.lastname == "" || formData.age == 0 || formData.grade == 0) {
+//     $('.info').empty();
+//     $('.info').append('<div class="alert alert-danger">Please enter student data to form</div>');
+//   } else {
+//     $.ajax({
+//       type: "POST",
+//       url: "/student",
+//       data: formData,
+//       dataType: "JSON",
+//       encode: true,
+//       headers: {
+//         "Authorization": "Bearer"
+//       }
+//     }).done(function () {
+//       $('.info').empty();
+//       getapi(url_students);
+//       $('input#formRegister__firstname').val('');
+//       $('input#formRegister__lastname').val('');
+//       $('input#formRegister__age').val('');
+//       $('input#formRegister__grade').val('');
+//       $('#exampleModal').hide();
+//       $('.close').click();
+//       $('.info').addClass("success");
+//       $('.info').append('<div class="alert alert-success">You have succesffully added a student!</div>');
+//     }).fail(function () {
+//       $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
+//     }).catch(e => {
+//       console.log(e)
+//     })
+//   }
+// })
+// // Delete Student
+// $(document).on('click', '#btn_delete', function (event) {
+//   event.preventDefault();
+//   var id = $(this).attr('data-id');
+//   $(document).on('click', '#delete-modal', function (event) {
+//     event.preventDefault();
+//     $('.info').empty();
+//     $.ajax({
+//       type: "DELETE",
+//       url: "/student/" + id,
+//       dataType: "JSON",
+//       encode: true,
+//       headers: {
+//         "Authorization": "Bearer"
+//       }
+//     }).done(function () {
+//       $('.info').empty();
+//       getapi(url_students);
+//       $('#exampleModal3').hide();
+//       $('.close').click();
+//       $('.info').addClass("success");
+//       $('.info').append('<div class="alert alert-danger">You have successfully deleted a student!</div>');
+//     }).fail(function () {
+//       $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
+//     }).catch(e => {
+//       console.log(e)
+//     })
+//   })
+// })
+// // Update Student
+// $(document).on('click', '#btn_update', function (event) {
+//   event.preventDefault();
+//   var id = $(this).attr('data-id');
+//   $(document).on('click', '#update-modal', function (event) {
+//     event.preventDefault();
+//     $('.info').empty();
+//     var formData = {
+//       "firstname": $('#myForm2').find('#formUpdate__firstname').val(),
+//       "lastname": $('#myForm2').find('#formUpdate__lastname').val(),
+//       "age": $('#myForm2').find('#formUpdate__age').val(),
+//       "grade": $('#myForm2').find('#formUpdate__grade').val(),
+//     };
+//     firstname.value = $('tbody.first tr').attr('data-id');
+//     if (formData.firstname == "" || formData.lastname == "" || formData.age == 0 || formData.grade == 0) {
+//       $('.info').empty();
+//       $('.info').append('<div class="alert alert-danger">Please enter student data to form</div>');
+//     } else {
+//       $.ajax({
+//         type: "PUT",
+//         url: "/student/" + id,
+//         dataType: "JSON",
+//         encode: true,
+//         headers: {
+//           "Authorization": "Bearer"
+//         },
+//         data: formData,
+//       }).done(function (data) {
+//         $('.info').empty();
+//         getapi(url_students);
+//         $('input#formUpdate__firstname').val('');
+//         $('input#formUpdate__lastname').val('');
+//         $('input#formUpdate__age').val('');
+//         $('input#formUpdate__grade').val('');
+//         $('#exampleModal2').hide();
+//         $('.close').click();
+//         $('.info').addClass("success");
+//         $('.info').append('<div class="alert alert-primary">You have successfully updated a student!</div>');
+//       }).fail(function () {
+//         $('.info').append('<div class="alert alert-danger">Invalid connection to the server!!!</div>');
+//       }).catch(e => {
+//         console.log(e);
+//       });
+//     }
+//   })
+// })
+// $(document).on('click', 'btn__register', function (e) {
+//   e.preventDefault();
+//   $(document).on('click', 'btn-register', function (e) {
+//     let user = {
+//       "username": $('form-register').find('formRegister__username').val(),
+//       "password": $('form-register').find('formRegister__password').val(),
+//       "email": $('form-register').find('formRegister__email').val(),
+//       "role": $('form-register').find('formRegister__role').val(),
+//     }
+//     $.ajax({
+//       type: 'POST',
+//       url: '/register',
+//       dataType: "json",
+//       data: JSON.stringify({ user }),
+//       encode: true,
+//       headers: {
+//         "Authorization": "Bearer"
+//       },
+//       success: function () {
+
+//       },
+//       error: function () {
+
+//       }
+//     }).catch(e => {
+//       console.log(e)
+//     })
+//   })
+// })
+// let registerUser = {
+//   username: document.getElementById('fusername').value(),
+//   password: document.getElementById('fpassword').value(),
+//   email: document.getElementById('fpassword').value(),
+//   role: document.getElementById('frole').value(),
+// };
