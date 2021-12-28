6ba33c3a57f728d29be820068237fc3ed3fab936
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-28 22:36:13 +0100

    [dev] - update token file

--- controllers/token.go
+++ controllers/token.go
@@ -13,13 +13,15 @@ import (
 func CreateAccessToken(userid uint64, role uint64, user models.User) *models.Token {
 	var token = &models.Token{}
 	token.Expiration = time.Now().Add(time.Minute * 15).Unix()
-
-	claims := jwt.MapClaims{}
-	claims["authorized"] = true
-	claims["role"] = models.GetUserRole(&user, userid)
-	claims["email"] = models.GetUserEmail(&user, userid)
-	claims["user_id"] = userid
-	claims["exp"] = token.Expiration
+	// create the claims
+	claims := jwt.MapClaims{
+		"authorized": true,
+		"role":       models.GetUserRole(&user, userid),
+		"email":      models.GetUserEmail(&user, userid),
+		"user_id":    userid,
+		"exp":        token.Expiration,
+	}
+	// create the JWT
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	token.Token, _ = at.SignedString([]byte(os.Getenv("ACCESS_SECRET_TOKEN")))
 	return token
@@ -28,10 +30,13 @@ func CreateAccessToken(userid uint64, role uint64, user models.User) *models.Tok
 func CreateRefreshToken(userid uint64, role uint64, user models.User) *models.Token {
 	var token = &models.Token{}
 	token.Expiration = time.Now().Add(time.Hour * 24 * 7).Unix()
-	claims := jwt.MapClaims{}
-	claims["role"] = models.GetUserRole(&user, userid)
-	claims["user_id"] = userid
-	claims["exp"] = token.Expiration
+	// create the claims
+	claims := jwt.MapClaims{
+		"role":    models.GetUserRole(&user, userid),
+		"user_id": userid,
+		"exp":     token.Expiration,
+	}
+	// create the jwt
 	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	token.Token, _ = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET_TOKEN")))
 	return token
@@ -53,7 +58,7 @@ func ExtractRefreshToken(stringToken string) (*jwt.Token, error) {
 	})
 }
 func RefreshToken(c *gin.Context) {
-	const BEARER_SCHEMA = "Bearer"
+	const BEARER_SCHEMA = "Bearer "
 	authHeader := c.GetHeader("Authorization")
 	stringToken := authHeader[len(BEARER_SCHEMA):]
 	token, err := ExtractRefreshToken(stringToken)
@@ -61,10 +66,12 @@ func RefreshToken(c *gin.Context) {
 		c.JSON(404, "Access deny!")
 		return
 	}
+	// cast claims to MapClaims
 	claims := token.Claims.(jwt.MapClaims)
+	// extract sub-claim
 	id := claims["user_id"].(uint64)
 	role := claims["role"].(uint64)
-
+	// create pair of tokens
 	newPairOfTokens := models.Tokens{}
 	newPairOfTokens.AccessToken = CreateAccessToken(id, role, models.User{})
 	newPairOfTokens.RefreshToken = CreateRefreshToken(id, role, models.User{})
