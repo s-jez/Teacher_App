4735aff65f9acfb4a721425f8802e21024c87e8d
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-28 22:36:54 +0100

    [dev] - remove comments

--- main.go
+++ main.go
@@ -33,24 +33,3 @@ func main() {
 	// Run server
 	r.Run(":8080")
 }
-
-//TODO dev branch:
-// 1 (frontend update)
-// use header json instead of form value
-// add to update current record values
-// 2 (dev branch)
-// JWT - Authentication Refresh Token and Access Token
-// Create account (register/login)
-// create tables in db and struct
-// add endpoints to backend
-// roles: admin (all permissions, view/edit/delete) / dev (view/edit)
-// anonymous (unlogged) users can only view users
-// authentication JWT
-// user login have access_token and refresh_token from backend
-// for next request from backend user send in header Autherization a access token
-// backend verify access token is avaible and for access token verify user (jwt - user_id)
-// for user_id we are searching if current user have role to do request from backend
-// if access_token expires it have to be refreshed from refresh_token
-// if refresh_token too expires, user must login again to account
-// authentication user in Middleware
-// at the end we can do unit tests
