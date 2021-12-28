5b8d9b315da6d7c43a820fede93ffb8c67f55f76
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-28 22:35:40 +0100

    [dev] - changes in students to BindJSON and delete FormValue

--- controllers/students.go
+++ controllers/students.go
@@ -2,21 +2,16 @@ package controllers
 
 import (
 	"Stachowsky/Teacher_App/models"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateStudent(c *gin.Context) {
 	var student models.Student
-	if err := c.Bind(&student); err != nil {
+	if err := c.BindJSON(&student); err != nil {
 		c.JSON(400, err.Error())
 		return
 	}
-	student.FirstName = c.Request.FormValue("firstname")
-	student.LastName = c.Request.FormValue("lastname")
-	student.Age, _ = strconv.Atoi(c.Request.FormValue("age"))
-	student.Grade, _ = strconv.Atoi(c.Request.FormValue("grade"))
 	if err := models.CreateStudent(&student); err != nil {
 		c.JSON(500, err.Error())
 		return
@@ -53,31 +48,20 @@ func ReadStudentById(c *gin.Context) {
 }
 func UpdateStudentById(c *gin.Context) {
 	var student models.Student
-	if err := c.Bind(&student); err != nil {
-		c.JSON(400, err.Error())
-		return
-	}
 	id := c.Param("id")
 	models.SelectById(&student, id)
 	if student.ID == 0 {
 		c.JSON(404, gin.H{"msg": "Student not found!"})
 	}
-
 	var nStudent models.Student
-	if err := c.Bind(&nStudent); err != nil {
+	if err := c.BindJSON(&nStudent); err != nil {
 		c.JSON(400, err.Error())
 		return
 	}
-
 	student.FirstName = nStudent.FirstName
 	student.LastName = nStudent.LastName
 	student.Age = nStudent.Age
 	student.Grade = nStudent.Grade
-
-	student.FirstName = c.Request.FormValue("firstname")
-	student.LastName = c.Request.FormValue("lastname")
-	student.Age, _ = strconv.Atoi(c.Request.FormValue("age"))
-	student.Grade, _ = strconv.Atoi(c.Request.FormValue("grade"))
 
 	if err := models.UpdateStudent(&student, id); err != nil {
 		c.JSON(500, err.Error())
@@ -88,10 +72,6 @@ func UpdateStudentById(c *gin.Context) {
 func DeleteStudentById(c *gin.Context) {
 	var student models.Student
 	id := c.Param("id")
-	if err := c.Bind(&student); err != nil {
-		c.JSON(400, err.Error())
-		return
-	}
 	if err := models.DeleteStudent(&student, id); err != nil {
 		c.JSON(500, err.Error())
 		return
