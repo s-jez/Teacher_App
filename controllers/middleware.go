32130c8fc91fc87f07004e30881565a0269dde7c
Stanisław Jeż <88712213+Stachowsky@users.noreply.github.com>
2021-12-30 11:15:45 +0100

    [dev] - middleware changes

--- controllers/middleware.go
+++ controllers/middleware.go
@@ -7,15 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware(isAuthenticatedRole []uint64) gin.HandlerFunc {
+func AuthMiddleware(Role []int) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
 			c.JSON(404, "No authorization header!")
 			c.Abort()
 		}
-		extractedToken := strings.Split(authHeader, "Bearer ")
-		stringToken := authHeader[len(extractedToken):]
+		extractedToken := strings.Split(authHeader, "Bearer")
+		if len(extractedToken) != 2 {
+			c.JSON(404, "Bearer token in not proper format")
+		}
+		stringToken := strings.TrimSpace(extractedToken[1])
 		token, err := ExtractAccessToken(stringToken)
 		if err != nil {
 			c.JSON(401, "No access to site!")
@@ -25,14 +28,14 @@ func AuthMiddleware(isAuthenticatedRole []uint64) gin.HandlerFunc {
 			c.AbortWithStatus(401)
 			return
 		}
-		if len(isAuthenticatedRole) > 0 {
-			RoleID := claims["role_id"].(uint64)
-			for _, v := range isAuthenticatedRole {
-				if RoleID == v {
+		if len(Role) > 0 {
+			RoleID := int(claims["role"].(float64))
+			for _, Value := range Role {
+				if Value == RoleID {
 					return
 				}
 			}
-			c.AbortWithStatus(404)
+			c.AbortWithStatus(403)
 			return
 		}
 	}
